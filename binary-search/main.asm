05d39c7a47cd869ec4b072853b1fa87027c35807
mehdi <mehdiadq@gmail.com>
2021-10-23 23:50:56 -0700

    bug fixed

--- binary-search/main.asm
+++ binary-search/main.asm
@@ -9,105 +9,117 @@ ARRAY_SIZE = 5
 
 .data
 	enterArrayPrompt byte "Enter array elements: ", 0
+	enterNumberToSearchPrompt byte "Enter number to search for: ", 0
+	notFound byte "not found", 0
+	foundAtStr byte "Found at: ", 0
+
 	array DWORD ARRAY_SIZE DUP(?)
-	enterNumberToSearchPrompt byte "Enter number to search for:", 0
+	
 	num DWORD ?
+	
 	l DWORD ?
 	r DWORD ?
 	m DWORD ?
 	i DWORD ?
-	notFound byte "not found", 0
+	
 
 .code
 
 main PROC
-	; Print enter array prompt
+	; Print 'enter array' prompt
 	mov edx, offset enterArrayPrompt
 	call writestring
 
 	; get array elements
-	mov edi, offset array
-	mov ecx, ARRAY_SIZE
+	mov edi, offset array						; edi = ptr to array
+	mov ecx, ARRAY_SIZE							; loop coounter
 
-	L1: call ReadInt
-		stosd
+	L1: call ReadInt							; read to eax
+		stosd									; move eax to next position of array (edi)
 		loop L1
 
-	; get target num
+	; print 'enter target' prompt
 	mov edx, offset enterNumberToSearchPrompt
 	call writestring
 
+	; get target num
 	call ReadInt
-	mov num, eax
+	mov num, eax								; num = eax -> contains target now
 
 	; initiate binary search
-	mov l, 0
-	mov r, ARRAY_SIZE-1
+	mov l, 0									; l = 0 the left boundry
+	mov r, ARRAY_SIZE-1							; r = len(array)-1 right boundry
 	
-	mov eax, l
-	mov ebx, r
-	cmp ebx, eax
-	JG L3
+L3:
+	mov eax, l									; eax = l
+	mov ebx, r									; ebx = r
+	cmp ebx, eax								; (l <= r) ?
+	JGE L7										; if (r >= l) jump L7
+	jmp NRet										; if (r < l) exit
 
-	L3:
-		mov eax, r
-		mov ebx, l
-		sub eax, ebx
-		sub edx, edx
-		mov ebx, 2
-		div ebx
-		add eax, l
-		mov m, eax
+	L7:
+		mov eax, r								; eax = r
+		mov ebx, l								; ebx = l
+		sub eax, ebx							; eax = eax - ebx
+		sub edx, edx							; edx = 0 need to do this because div is edx:eax / ebx
+		mov ebx, 2								; ebx = 2
+		div ebx									; eax = eax / 2
+		add eax, l								; eax = l
+		mov m, eax								; m = l + (r - l) / 2
 
-		mov esi, offset array
-		mov eax, m
-		mov ebx, 4
-		mul ebx
-		mov ebx, [esi+eax]
-		mov i, eax
-		mov eax, num
-		cmp eax, ebx
-		jne L4
-
-		jmp L6
+		mov esi, offset array					; esi = ptr to array
+		mov eax, m								; eax = m
+		mov ebx, 4								; ebx = 4
+		mul ebx									; eax = m * 4
+		mov ebx, [esi+eax]						; ebx = array[m]
+		mov i, eax								; i = m * 4 for getting the index later
+		mov eax, num							; eax = num
+		cmp eax, ebx							; array[m] == num ?
+		jne L4									; if (array[m] != num) jump L4
+		jmp L6									; if (array[m] == num) yay! we found it!
 		
-		L4:
-			mov esi, offset array
-			mov eax, m
-			mov ebx, 4
-			mul ebx
-			mov ebx, [esi+eax]
-			mov eax, num
-			cmp ebx, eax
-			jg L5
-			mov eax, m
-			add eax, 1
-			mov l, eax
-			jmp L3
+	L4:
+		mov esi, offset array					; esi = ptr to array
+		mov eax, m								; eax = m
+		mov ebx, 4								; ebx = 4
+		mul ebx									; eax = m * 4
+		mov ebx, [esi+eax]						; ebx = array[m]
+		mov eax, num							; eax = num
+		cmp ebx, eax							; num > array[m] ?
+		jg L5									; if (num < array[m]) jump L5
+		
+		; if (num > array[m])
+		mov eax, m								; eax = m
+		add eax, 1								; eax = m + 1
+		mov l, eax								; l = m + 1
+		jmp L3									; Loop
 
-			L5:
-				mov eax, m
-				sub eax, 1
-				mov r, eax
-				jmp L3
+	L5:
+		mov eax, m								; eax = m
+		sub eax, 1								; eax = m - 1
+		mov r, eax								; r = m - 1
+		jmp L3							     	; Loop
 				
 
-		jmp NRet
+	jmp NRet
 
-		; found
-		L6:
-		mov eax, i
-		mov ebx, 4
-		div ebx
-		call writeint
-		jmp e
+	; found
+	L6:
+		mov eax, i								; eax = index * 4
+		mov ebx, 4								; ebx = 4
+		div ebx									; eax = index
+		mov edx, offset foundAtStr				; edx = ptr to foundAtStr
+		call writestring
+		call writeint							; write found index
+		jmp e									; exit
 
-		; not found
-		NRet:
-			mov edx, offset notFound
-			call writestring
+	; print 'not found'
+	NRet:
+		mov edx, offset notFound				
+		call writestring
 
-		e:
+	; exit
+	e:
 
 	INVOKE ExitProcess, 0
 main ENDP
